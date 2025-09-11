c90b6911872ad912479691c3dccb756ed6bb3fe5
gabriel-castro <gabrielcastro.developer@gmail.com>
2025-09-11 20:00:10 -0300

    somar até 10 e zerar contador

--- src/app/pages/counter/counterPage.component.ts
+++ src/app/pages/counter/counterPage.component.ts
@@ -7,13 +7,30 @@ import { Component } from '@angular/core';
   styleUrl: './counterPage.component.scss'
 })
 export class CounterPageComponent {
+  // O valor sempre será atualizado na tela pois
+  // foi feita a interpolação do mesmo em um elemento HTML
   counter: number = 0;
 
-  somar(){
-    this.counter++;
+  somar(): number{
+    // Aumenta e retorna o valor do contador
+    return ++this.counter;
   }
 
-  diminuir(){
-    this.counter--;
+  diminuir(): number{
+    // Diminui e retorna o valor do contador
+    return --this.counter;
+  }
+
+  somarAte10(){
+    const valorAtual = this.somar(); // chama a função de soma efetivamente
+
+    // Reseta o contador caso tennha passado de 10
+    if(valorAtual > 10){
+      this.counter = 0;
+    }
+  }
+
+  zerarContador(){
+    this.counter = 0;
   }
 }
