5687feeea2e7a26591f593346f6e8f33aebd963e
Gabriel Castro <gabrielcastro.developer@gmail.com>
2025-09-15 13:29:56 -0300

    lista de compras

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { IndexComponent } from './pages/index/index.component';
 import { CounterPageComponent } from './pages/counter/counterPage.component'
+import { ListaComprasComponent } from './components/lista-compras/lista-compras.component';
 
 export const routes: Routes = [
     { path: 'calculadora', component: IndexComponent },
     { path: '', redirectTo: '/calculadora', pathMatch: 'full' },
-    { path: 'counter', component: CounterPageComponent }
+    { path: 'counter', component: CounterPageComponent },
+    { path: 'compras', component: ListaComprasComponent }
 ];
