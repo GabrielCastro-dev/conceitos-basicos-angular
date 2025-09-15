478d26ba433fe062f7441ba78f0b251e2a1cef82
Gabriel Castro <gabrielcastro.developer@gmail.com>
2025-09-15 13:43:14 -0300

    listra de compras com formulário funcional

--- src/app/components/lista-compras/lista-compras.component.ts
+++ src/app/components/lista-compras/lista-compras.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-lista-compras',
-  imports: [],
+  imports: [ FormsModule ],
   templateUrl: './lista-compras.component.html',
   styleUrl: './lista-compras.component.scss'
 })
 export class ListaComprasComponent {
+  novoItem: string = '';
 
+  adicionarItem(){
+    console.log("Item recebido: " + this.novoItem);
+  }
 }
