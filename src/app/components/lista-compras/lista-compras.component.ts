fc12091445440a65b41584c7ef1e6c20ef59b51b
Gabriel Castro <gabrielcastro.developer@gmail.com>
2025-09-16 10:03:35 -0300

    mapeamento de listas

--- src/app/components/lista-compras/lista-compras.component.ts
+++ src/app/components/lista-compras/lista-compras.component.ts
@@ -1,16 +1,24 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ItemLista } from './ItemLista';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-lista-compras',
-  imports: [ FormsModule ],
+  imports: [ FormsModule, CommonModule ],
   templateUrl: './lista-compras.component.html',
   styleUrl: './lista-compras.component.scss'
 })
 export class ListaComprasComponent {
   novoItem: string = '';
 
+  lista: ItemLista[] = []; // Array de ItemLista
+
   adicionarItem(){
-    console.log("Item recebido: " + this.novoItem);
+    let item = new ItemLista();
+    item.id = this.lista.length + 1;
+    item.nome = this.novoItem;
+    
+    this.lista.push(item);
   }
 }
